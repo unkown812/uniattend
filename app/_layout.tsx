4c749d3f190e48510b4e805bf0d01467baf68db3
unkown812 <jayjogane@gmail.com>
2025-08-07 22:24:41 +0530

    modified:   app/_layout.tsx 	new file:   assets/fonts/PlaywriteUSModern-VariableFont_wght.ttf

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ClashDisplay: require('../assets/fonts/ClashDisplay-Variable.ttf'),
     ClashGrostek: require('../assets/fonts/ClashGrotesk-Variable.ttf'),
+    Playwrite: require('../assets/fonts/PlaywriteUSModern-VariableFont_wght.ttf'),
   });
 
   if (!loaded) {
@@ -19,19 +20,18 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="splash" options={{ headerShown: false }} />
-        <Stack.Screen name="login-selection" options={{ headerShown: false }} />
-        <Stack.Screen name="student-signin" options={{ headerShown: false }} />
-        <Stack.Screen name="teacher-signin" options={{ headerShown: false }} />
-        <Stack.Screen name="teacher-login" options={{ headerShown: false }} />
-        <Stack.Screen name="subjects-teachers" options={{ headerShown: false }} />
-        <Stack.Screen name="subjects-students" options={{ headerShown: false }} />
-        <Stack.Screen name="subject-detail" options={{ headerShown: false }} />
-        <Stack.Screen name="students" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="subjects-student" options={{ headerShown: false }} /> */}
-        <Stack.Screen name="subject-mark-attendance" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)/index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
+        <Stack.Screen name="login-selection" options={{ headerShown: false }} />
+        <Stack.Screen name="splash" options={{ headerShown: false }} />
+        <Stack.Screen name="student-signin" options={{ headerShown: false }} />
+        <Stack.Screen name="students" options={{ headerShown: false }} />
+        <Stack.Screen name="subject-detail" options={{ headerShown: false }} />
+        <Stack.Screen name="subject-mark-attendance" options={{ headerShown: false }} />
+        <Stack.Screen name="subjects-students" options={{ headerShown: false }} />
+        <Stack.Screen name="subjects-teachers" options={{ headerShown: false }} />
+        <Stack.Screen name="teacher-signin" options={{ headerShown: false }} />
+        <Stack.Screen name="teacher-login" options={{ headerShown: false }} />
       </Stack>
     </ThemeProvider>
   );
